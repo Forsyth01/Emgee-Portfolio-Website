a4e2b507f8db7d97625ff247410ca9b318d66610
FORSYTH OKOEGUALE <forsythokoeguale01@gmail.com>
2025-10-04 08:14:58 +0100

    new commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
   {
     id: 1,
     title: "Daily Food",
-    description: "Crypto to Naira and VTU App",
+    description: "Food Vendor & Marketplace App (UI/UX Design)",
     coverImage: "/images/projects/dailyfood/dailyfood8.png",
     images: [
       { src: "/images/projects/dailyfood/dailyfood1.png", span: true },
@@ -21,7 +21,13 @@ export const projects = [
       // { name: "CorelDRAW", icon: "/assets/icons/coreldraw.png" },
     ],
     roleDescription:
-      "Mintrix Trade is a digital trading platform that allows users to securely buy and sell cryptocurrencies and gift cards for Naira. The mobile app was designed to provide a fast, intuitive, and secure trading experience for both new and experienced users.",
+      `I contributed to the design of Dailyfood, a food vending and marketplace app. While I wasn’t responsible for the entire product, I designed key features that added unique value to the platform:
+- Asusu Feature: A savings subscription that allows users to plan and save toward bulk food purchases like a carton of noodles, a bag of rice, or semo. Plans included Flex Combo, Flex Pro, Super Flex, and Super Pro.
+- Food Subscription Plans: Weekly and monthly subscription options for convenient meal planning.
+- AI Features: Designed twin AI features – AI Meal Plan and AI Suggestion – to personalize user experiences and help users discover or plan meals.
+
+These designs made the app more versatile, bridging everyday food needs with smart financial planning.
+`,
     link: "https://myportfolio.com",
     date: "March, 2025",
   },
@@ -29,7 +35,7 @@ export const projects = [
   {
     id: 2,
     title: "Mintrix Trade App",
-    description: "Crypto to Naira and VTU App",
+    description: "Mobile App (UI/UX Design)",
     coverImage: "/images/projects/mintrix/mintrix1.png",
     images: [
       { src: "/images/projects/mintrix/mintrix1.png", span: false },
@@ -51,7 +57,7 @@ export const projects = [
       // { name: "CorelDRAW", icon: "/assets/icons/coreldraw.png" },
     ],
     roleDescription:
-      "Mintrix Trade is a digital trading platform that allows users to securely buy and sell cryptocurrencies and gift cards for Naira. The mobile app was designed to provide a fast, intuitive, and secure trading experience for both new and experienced users.",
+      "I designed the user interface for Mintrix Trade Limited’s mobile app, a fintech platform that allows users to trade cryptocurrencies (BTC to Naira) and exchange gift cards for cash. Beyond crypto and gift cards, the app also integrates bill payments, sports betting funding, and flight booking, making it an all-in-one finance and lifestyle solution. My role focused on creating a clean, intuitive user experience that makes trading and transactions simple and secure.",
     link: "https://myportfolio.com",
     date: "March, 2025",
   },
@@ -59,16 +65,16 @@ export const projects = [
   {
     id: 3,
     title: "Pozse",
-    description: "A Fashion Tech Landing Page",
+    description: "Landing Page (UI/UX Design)",
     coverImage: "/images/projects/posze.png",
-    images: [{ src: "/images/projects/pozse/pozse1.png", span: true}],
-    images: [{ src: "/images/projects/pozse/pozse2.png", span: true}],
-    images: [{ src: "/images/projects/pozse/pozse3.png", span: true}],
-    images: [{ src: "/images/projects/pozse/pozse4.png", span: true}],
+    images: [{ src: "/images/projects/pozse/pozse1.png", span: true }],
+    images: [{ src: "/images/projects/pozse/pozse2.png", span: true }],
+    images: [{ src: "/images/projects/pozse/pozse3.png", span: true }],
+    images: [{ src: "/images/projects/pozse/pozse4.png", span: true }],
     roles: ["UI/UX Designer"],
     tools: [{ name: "Figma", icon: "/assets/icons/figma.png" }],
     roleDescription:
-      "Designed a modern landing page with clear CTAs and engaging visuals. Focused on branding, layout consistency, and responsive UI.",
+      "Pozse is a brand that merges fashion and technology. I designed their landing page, which served as the first digital touchpoint for the brand. The landing page not only showcased the brand’s vision but also included a waitlist form to collect early sign-ups and build community interest ahead of their main product launch (a social media app and other extensions). The design helped position Pozse as both innovative and stylish",
     link: "https://myecommerce.com",
     date: "July, 2024",
   },
@@ -76,7 +82,7 @@ export const projects = [
   {
     id: 4,
     title: "Mintrix Admin Panel",
-    description: "Admin Management System",
+    description: "Admin Dashboard (UI/UX Design)",
     coverImage: "/images/projects/mintrixadmin.png",
     images: [
       { src: "/images/projects/mintrix1.png", span: true },
@@ -87,7 +93,7 @@ export const projects = [
     roles: ["UI/UX Designer"],
     tools: [{ name: "Figma", icon: "/assets/icons/figma.png" }],
     roleDescription:
-      "Worked on dashboards and admin flows with emphasis on usability, data visualization, and efficient management screens.",
+      "As a continuation of the mobile app, I also designed the Mintrix Trade Admin Dashboard. The dashboard mirrors the functionality of the mobile app but from the administrative perspective. It allows admins to manage user accounts, monitor transactions, process trades, and oversee services like bill payments and flight bookings. I ensured the dashboard maintained the same design consistency and usability as the mobile app, while giving admins the tools they need for smooth operations.",
     link: "https://myweatherapp.com",
     date: "May, 2025",
   },
@@ -95,7 +101,7 @@ export const projects = [
   {
     id: 5,
     title: "Geniz Global",
-    description: "Social Media Flyers for Tech Agency.",
+    description: "Social Media Flyers (Graphic Design)",
     coverImage: "/images/projects/geniz.png",
     images: [
       { src: "/images/projects/genix/genix1.png", span: false },
@@ -113,7 +119,7 @@ export const projects = [
       // { name: "CorelDRAW", icon: "/assets/icons/coreldraw.png" },
     ],
     roleDescription:
-      "Created high-quality social media flyers for campaigns, focusing on visual appeal and brand consistency.",
+      "Geniz Tech, a digital agency, reached out for social media flyer designs both for their own branding and their clients. I created visuals that highlighted their goals and services, helping them build credibility and a stronger online presence.",
     link: "https://geniz.com",
     date: "March, 2025",
   },
@@ -171,7 +177,7 @@ export const projects = [
   {
     id: 8,
     title: "Iprospa",
-    description: "Crypto and giftcard trading brand",
+    description: "Social Media Flyers (Graphic Design)",
     coverImage: "/images/projects/Iprospa/iprospa.png",
     images: [
       { src: "/images/projects/Iprospa/iprospa2.png", span: false },
@@ -186,7 +192,7 @@ export const projects = [
       { name: "Photoshop", icon: "/assets/icons/photoshop.png" },
     ],
     roleDescription:
-      "Led the UI revamp of the telecom dashboard, ensuring better user flows and an updated design system.",
+      "For iProspa, a brand that trades crypto and gift cards to naira, I handled the graphic design for their social media flyers. The designs were visually striking, aligned with their brand identity, and effectively communicated their services to their target audience.",
     link: "https://embdashboard.com",
     date: "March, 2025",
   },
@@ -194,7 +200,7 @@ export const projects = [
   {
     id: 9,
     title: "Travis B",
-    description: "Crypto and giftcard trading brand",
+    description: " Graphic Design",
     coverImage: "/images/projects/travisb/travisb1.png",
     images: [
       { src: "/images/projects/travisb/travisb1.png", span: false },
@@ -206,14 +212,14 @@ export const projects = [
     roles: ["Graphic Designer"],
     tools: [{ name: "Photoshop", icon: "/assets/icons/photoshop.png" }],
     roleDescription:
-      "Led the UI revamp of the telecom dashboard, ensuring better user flows and an updated design system.",
+      "For Travis B Exchange, a crypto and gift card trading brand, I created social media flyers to promote their services. The flyers communicated their value clearly while maintaining a professional and engaging look tailored for their online audience.",
     link: "https://embdashboard.com",
     date: "March, 2025",
   },
   {
     id: 10,
     title: "Skillet",
-    description: "UI Revamp for EMB Telecomms",
+    description: "Concept Design (UI/UX)",
     coverImage: "/images/projects/skillet/skillet.png",
     images: [
       { src: "/images/projects/skillet/skillet1.png", span: true },
@@ -221,30 +227,30 @@ export const projects = [
       { src: "/images/projects/skillet/skillet3.png", span: true },
     ],
     roles: ["UI/UX Designer"],
-    tools: [  { name: "Figma", icon: "/assets/icons/figma.png" },],
+    tools: [{ name: "Figma", icon: "/assets/icons/figma.png" },],
     roleDescription:
-      "Led the UI revamp of the telecom dashboard, ensuring better user flows and an updated design system.",
+      "Skillet is a personal project I developed to practice and showcase my UI/UX design skills. I designed the concept end-to-end, ensuring it reflects my ability to craft user-friendly, modern interfaces while experimenting with creative layouts and visual storytelling.",
     link: "https://embdashboard.com",
     date: "March, 2025",
   },
   {
     id: 11,
     title: "Artist Profile Design",
-    description: "Crypto to Naira and VTU App",
+    description: "Finn YMD (Graphic Design)",
     coverImage: "/images/projects/artist/artist1.png",
-    images:[
-      { src: "/images/projects/artist/artist1.png", span:true },
-      { src: "/images/projects/artist/artist2.png", span:true },
-      { src: "/images/projects/artist/artist3.png", span:true },
-      { src: "/images/projects/artist/artist4.png", span:true },
-      { src: "/images/projects/artist/artist5.png", span:true },
-      { src: "/images/projects/artist/artist6.png", span:true },
-      { src: "/images/projects/artist/artist7.png", span:true },
-      { src: "/images/projects/artist/artist8.png", span:true },
-      { src: "/images/projects/artist/artist9.png", span:true },
-      { src: "/images/projects/artist/artist10.png", span:true },
-      { src: "/images/projects/artist/artist11.png", span:true },
-      { src: "/images/projects/artist/artist12.png", span:true },
+    images: [
+      { src: "/images/projects/artist/artist1.png", span: true },
+      { src: "/images/projects/artist/artist2.png", span: true },
+      { src: "/images/projects/artist/artist3.png", span: true },
+      { src: "/images/projects/artist/artist4.png", span: true },
+      { src: "/images/projects/artist/artist5.png", span: true },
+      { src: "/images/projects/artist/artist6.png", span: true },
+      { src: "/images/projects/artist/artist7.png", span: true },
+      { src: "/images/projects/artist/artist8.png", span: true },
+      { src: "/images/projects/artist/artist9.png", span: true },
+      { src: "/images/projects/artist/artist10.png", span: true },
+      { src: "/images/projects/artist/artist11.png", span: true },
+      { src: "/images/projects/artist/artist12.png", span: true },
     ],
     roles: ["UI/UX Designer"],
     tools: [
@@ -252,7 +258,7 @@ export const projects = [
       // { name: "CorelDRAW", icon: "/assets/icons/coreldraw.png" },
     ],
     roleDescription:
-      "Mintrix Trade is a digital trading platform that allows users to securely buy and sell cryptocurrencies and gift cards for Naira. The mobile app was designed to provide a fast, intuitive, and secure trading experience for both new and experienced users.",
+      "I designed a professional artist profile for Finn YMD, which included his biography, discography, and other essential details. The design provided a clean and engaging way to present the artist’s journey and music catalog, blending both branding and storytelling.",
     link: "https://myportfolio.com",
     date: "March, 2025",
   },
